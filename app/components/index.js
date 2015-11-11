e470c4c5c8541cd9000aebcba6bee056062c0bb9
Anthony Bluemmel <tonyxblues@me.com>
2015-11-11 17:12:59 -0500

    idk

--- app/components/index.js
+++ app/components/index.js
@@ -12,84 +12,83 @@ import _ from 'underscore';
 var Index = React.createClass({
 
 propTypes: {
-  locations: React.PropTypes.object
+ locations: React.PropTypes.object
 },
 
 mixins: [BackboneMixin],
 
 getDefaultProps() {
-  return {
-    locations: store.getLocation()
-  };
+ return {
+   locations: store.getLocation()
+ };
 },
 
 componentWillMount() {
-  this.props.locations.on('add change remove', this.forceUpdate.bind(this, null), this);
-  navigator.geolocation.getCurrentPosition((position) => {
-    this.props.locations.setLocation(position.coords.latitude, position.coords.longitude);
-    this.props.locations.fetch({success: () => {
-    }})
-  })
+ this.props.locations.on('add change remove', this.forceUpdate.bind(this, null), this);
+ navigator.geolocation.getCurrentPosition((position) => {
+   this.props.locations.setLocation(position.coords.latitude, position.coords.longitude);
+   this.props.locations.fetch({success: () => {
+   }})
+ })
 },
 
 componentWillUnmount() {
-  this.props.locations.off('add change remove', null, this);
+ this.props.locations.off('add change remove', null, this);
 },
 
 getModels() {
-  return {
-    restaurants: store.getRestaurants()
-  };
+ return {
+   restaurants: store.getRestaurants()
+ };
 },
 
 handleSubmit(e) {
-  e.preventDefault();
-  let search = this.refs.search.value;
-  store.searchRestaurants(search);
-  console.log(search);
+ e.preventDefault();
+ let search = this.refs.search.value;
+ store.searchRestaurants(search);
+ console.log(search);
 },
 
 randomizeRestaurant(e) {
-  console.log("hello");
-
-  e.preventDefault();
-  var sample = _.sample(this.state.locations);
-  console.log(sample);
-  this.setState({
-    randomRest: sample
-  })
+ e.preventDefault();
+ var sample = _.sample(this.locations);
+ console.log(sample);
+ console.log(this.locations);
+ this.setState({
+   randomRest: sample
+ })
 },
 
-  render() {
-    var hasLocations;
-    var locations;
-    if (this.props.locations.length > 0) {
-      hasLocations = true;
-      locations = _.map(this.props.locations.models[0].attributes.nearby_restaurants);
-    } else {
-      hasLocations = false;
-    }
+ render() {
+   var hasLocations;
+   if (this.props.locations.length > 0) {
+     hasLocations = true;
+     this.locations = _.map(this.props.locations.models[0].attributes.nearby_restaurants);
+   } else {
+     hasLocations = false;
+   }
 
-    return (
-      <div>
-        <nav className="top-nav2">
-          <Link to="/profile"><button className="profile-button"><i className="fa fa-user"></i></button></Link>
-          <Link to="/search"><button className="search-button"><i className="fa fa-search"></i></button></Link>
-        </nav>
-        <button onCLick={this.randomizeRestaurant} className="randomize-button">Random</button>
-          {this.state.randomRest &&
-            <li>{this.state.randomRest.restaurant.name}</li>
 
-          }
-          {hasLocations &&
-            <div>
-              {locations.map((result)=><div className="search-results" key={result.restaurant.R.res_id}><Link to={`/restaurant/${result.restaurant.id}`}><li className="r-name">{result.restaurant.name}</li> <li className="r-cuisine">{result.restaurant.cuisines}</li><li className="r-cost">{result.restaurant.currency}{result.restaurant.average_cost_for_two}</li>
-            <li className="r-location">{result.restaurant.location.city}</li><li className="r-rating">{result.restaurant.user_rating.aggregate_rating}</li></Link><li className="r-address"><a className="fa fa-crosshairs" href=""></a></li></div> )}
-            </div>
-          }
-      </div>
-    );
-  }
+   return (
+     <div>
+       <nav className="top-nav2">
+         <Link to="/profile"><button className="profile-button"><i className="fa fa-user"></i></button></Link>
+         <Link to="/search"><button className="search-button"><i className="fa fa-search"></i></button></Link>
+       </nav>
+       <button onClick={this.randomizeRestaurant} className="randomize-button">Random</button>
+         {this.state.randomRest &&
+           <li>{this.state.randomRest.restaurant.name}</li>
+
+         }
+         {hasLocations &&
+           <div>
+             {this.locations.map((result)=><div className="search-results" key={result.restaurant.R.res_id}><Link to={`/restaurant/${result.restaurant.id}`}><li className="r-name">{result.restaurant.name}</li> <li className="r-cuisine">{result.restaurant.cuisines}</li><li className="r-cost">{result.restaurant.currency}{result.restaurant.average_cost_for_two}</li>
+           <li className="r-location">{result.restaurant.location.city}</li><li className="r-rating">{result.restaurant.user_rating.aggregate_rating}</li></Link><li className="r-address"><a className="fa fa-crosshairs" href=""></a></li></div> )}
+           </div>
+         }
+     </div>
+   );
+ }
 });
 
 export default Index;
